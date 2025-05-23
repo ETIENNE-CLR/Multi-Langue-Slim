65b77f87e2b24bb2de4c4d849d3ab9c752cb3eb2
Etienne Caulier <etienne.clr@eduge.ch>
2025-05-23 09:23:42 +0200

    maj test queries

--- src/config/queries.sql
+++ src/config/queries.sql
@@ -26,3 +26,33 @@ WHERE id = :id
 
 -- Delete
 DELETE FROM Personne WHERE id = :id
+
+
+
+
+
+-- TRADUCTION --
+
+SELECT * FROM PersonneActivities.Activite_traduction WHERE lang = 'fr';
+
+-- Read principal
+SELECT
+	Personne.id,
+    Personne.nom,
+    Personne.prenom,
+    Personne.dateNaissance,
+    Localite.nom AS `Localite`,
+    Personne.depuis
+FROM PersonneActivities.Personne
+JOIN PersonneActivities.Localite ON Personne.idLocalite = Localite.id;
+            
+-- Récupérer toutes les activités d'un user dans une langue précise
+SELECT Activite_traduction.nom
+FROM Activite_traduction
+JOIN Pratique ON Activite_traduction.id = Pratique.idActivite
+JOIN Personne ON Pratique.idPersonne = Personne.id
+JOIN Langue ON Activite_traduction.lang = Langue.lang
+WHERE Personne.id = 1 AND Langue.lang = "fr";
+
+SELECT *
+FROM PersonneActivities.Activite_traduction;
